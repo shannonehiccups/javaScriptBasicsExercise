13b2ca5639f1a06496822c537a066c437d64ef62
Shannon Hicks <shannonehiccups@gmail.com>
2021-09-20 15:21:10 -0500

    JavaScript Basics Exercise

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log(42);
+// The 5 primitives: Numbers, Strings, Booleans, Null, Undefined
+// 6 operator symbols: +, -, *, /, **, %
+console.log(5%2);
+console.log(5%4);
+console.log('I AM MARY POPPINS Y\'ALL!');
+console.log("Hawk"+"eye");
+console.log("Shannon Hicks".length);
+console.log('"Avengers: Endgame"'[17]);
